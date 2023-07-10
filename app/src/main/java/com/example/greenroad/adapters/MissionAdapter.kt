ea665309f4117dbec30f6f704011ab8dba687d83
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2023-07-10 22:16:31 +0300

    Added adapter for missions.

---
+++ app/src/main/java/com/example/greenroad/adapters/MissionAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.greenroad.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.greenroad.data.model.Location
+import com.example.greenroad.databinding.MissionItemBinding
+
+class MissionAdapter : RecyclerView.Adapter<MissionAdapter.ViewHolder>() {
+
+     private var locationList : List<Location> = listOf()
+    fun setList(locationList : List<Location>){
+        this.locationList=locationList
+        notifyDataSetChanged()
+    }
+    inner class ViewHolder(val binding : MissionItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = MissionItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        with(holder){
+            with(locationList[position]){
+                binding.locationName.text = this.name
+                binding.locationLatitude.text = this.latitude.toString()
+                binding.locationLongitude.text = this.longitude.toString()
+                binding.locationPoint.text = this.point.toString()
+
+
+            }
+        }
+
+
+    }
+
+
+    override fun getItemCount(): Int {
+        return locationList.size
+    }
+
+
+}
--- app/src/main/java/com/example/greenroad/ui/view/mission/MissionFragment.kt
+++ app/src/main/java/com/example/greenroad/ui/view/mission/MissionFragment.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.greenroad.R
+import com.example.greenroad.adapters.MissionAdapter
+import com.example.greenroad.databinding.FragmentMissionBinding
+import com.example.greenroad.ui.view.map.MapViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class MissionFragment : Fragment() {
+
+    private val mapViewModel: MapViewModel by viewModels()
+    private lateinit var missionAdapter: MissionAdapter
+    private var binding: FragmentMissionBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +31,25 @@ class MissionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mission, container, false)
+        binding = FragmentMissionBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding!!.missionRecyclerView.layoutManager = LinearLayoutManager(context)
+        missionAdapter = MissionAdapter()
+
+        mapViewModel.getDataFromFirebase()
+        mapViewModel.locationList.observe(viewLifecycleOwner) {
+            println("Buraya geldi" + it.size)
+            missionAdapter.setList(it)
+        }
+        binding!!.missionRecyclerView.adapter = missionAdapter
+
+
     }
 
 
